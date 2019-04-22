9277c1a0bfd485bd300dc2d2d9706964f86ab4ce
Unknown <skyvolga@mail.ru>
2019-04-22 20:27:05 +1200

    Material-UI was added

--- openli/src/index.js
+++ openli/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,500', 'Material+Icons']
+  }
+});
